5dc0b6a3c376389357ef60221d43a822034d66f0
jiangji <774133054@qq.com>
2019-11-05 00:37:47 +0800

    配合nginx配置，改变接口路径

--- src/url/index.js
+++ src/url/index.js
@@ -1,5 +1,6 @@
 // const baseUrl = 'http://127.0.0.1:8080/api'
-const baseUrl = 'http://localhost:8080/api'
+// const baseUrl = 'http://localhost:8080/api'
+const baseUrl = '/api'
 
 const url =  {
   list: `${baseUrl}/list`,
